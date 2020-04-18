30d9d68a657f0f74d774016489a298f08f6d0068
chenwei liu <a500667337@gmail.com>
2020-04-18 18:07:05 +0000

    練習完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: '一棵樹',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: '今天看到一顆整齊的樹'
+    },
+    {
+      title: '雪山',
+      imageUrl: 'assets/mountain.jpeg',
+      username: '登山愛好者',
+      content: '這是一張雪山的照片'
+    },
+    {
+      title: '山地自行車',
+      imageUrl: 'assets/biking.jpeg',
+      username: '三地',
+      content: '一台自行車'
+    },
+    {
+      title: '山地自行車',
+      imageUrl: 'assets/biking.jpeg',
+      username: '三地',
+      content: '一台自行車'
+    }
+  ];
 }
